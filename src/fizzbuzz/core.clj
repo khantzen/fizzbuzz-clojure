a4ff58f78e6b2347db324ecc1fab46b08ba6be59
Kevin Hantzen <kevin.hantzen@gmail.com>
2020-05-17 22:42:52 +0200

    format files

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -4,27 +4,22 @@
 (defn returnValue [orDefault] #(:then_return % orDefault))
 
 (defn create-fizzBuzz [fizzBuzzRules]
-    (fn fb? 
-        ([arg] 
-            (->
-                (filter #( (validate %)  arg) fizzBuzzRules)
-                (first)
-                ((returnValue (str arg)))
-            )
-        )
-        ([arg & next] (str (fb? arg) " " (apply fb? next)))
-    )
-)
+  (fn fb?
+    ([arg]
+     (->
+      (filter #((validate %)  arg) fizzBuzzRules)
+      (first)
+      ((returnValue (str arg)))))
+
+    ([arg & next] (str (fb? arg) " " (apply fb? next)))))
 
 (defn create_divide_by [value] #(= 0 (mod % value)))
 (def divBy3? (create_divide_by 3))
 (def divBy5? (create_divide_by 5))
 
 (def classic-fizzBuzz
-    [{:if_validate  #(and (divBy5? %) (divBy3? %))       :then_return "FizzBuzz"}
-     {:if_validate  #(and (divBy3? %) (not (divBy5? %))) :then_return "Fizz"}
-     {:if_validate  #(and (divBy5? %) (not (divBy3? %))) :then_return "Buzz"}
-    ]
-)
+  [{:if_validate  #(and (divBy5? %) (divBy3? %))       :then_return "FizzBuzz"}
+   {:if_validate  #(and (divBy3? %) (not (divBy5? %))) :then_return "Fizz"}
+   {:if_validate  #(and (divBy5? %) (not (divBy3? %))) :then_return "Buzz"}])
 
 (def fizzBuzz (create-fizzBuzz classic-fizzBuzz))
--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -3,36 +3,31 @@
             [fizzbuzz.core :refer :all]))
 
 (deftest divisible-by-three-should-yield-fizz
-    (testing "12 should return Fizz"
-      (is (= "Fizz" (fizzBuzz 12))))
-    (testing "18 should return Fizz" 
-      (is (= "Fizz" (fizzBuzz 18))))
-)
+  (testing "12 should return Fizz"
+    (is (= "Fizz" (fizzBuzz 12))))
+  (testing "18 should return Fizz"
+    (is (= "Fizz" (fizzBuzz 18)))))
 
 (deftest divisible-by-5-should-yield-buzz
-    (testing "5 should return Buzz"
-        (is (= "Buzz" (fizzBuzz 5))))
-    (testing "10 should return Buzz"
-        (is (= "Buzz" (fizzBuzz 10))))
-)
+  (testing "5 should return Buzz"
+    (is (= "Buzz" (fizzBuzz 5))))
+  (testing "10 should return Buzz"
+    (is (= "Buzz" (fizzBuzz 10)))))
 
 (deftest divisible-neither-by-3-nor-5-should-yield-input
-    (testing "8 should return 8"
-        (is (= "8" (fizzBuzz 8))))
-    (testing "11 should return 11"
-        (is (= "11" (fizzBuzz 11))))
-        (testing "14 should return 14"
-            (is (= "14" (fizzBuzz 14))))
-)
+  (testing "8 should return 8"
+    (is (= "8" (fizzBuzz 8))))
+  (testing "11 should return 11"
+    (is (= "11" (fizzBuzz 11))))
+  (testing "14 should return 14"
+    (is (= "14" (fizzBuzz 14)))))
 
 (deftest divisible-by-3-and-five-should-yield-FizzBuzz
-    (testing "15 should return FizzBuzz"
-        (is (= "FizzBuzz" (fizzBuzz 15))))
-    (testing "30 should return FizzBuzz"
-        (is (= "FizzBuzz" (fizzBuzz 30))))
-)
+  (testing "15 should return FizzBuzz"
+    (is (= "FizzBuzz" (fizzBuzz 15))))
+  (testing "30 should return FizzBuzz"
+    (is (= "FizzBuzz" (fizzBuzz 30)))))
 
 (deftest should-convert-a-list-of-value-to-FizzBuzz
-    (testing "15 22 10 9 should returr FizzBuzz 22 Buzz Fizz")
-        (is (= "FizzBuzz 22 Buzz Fizz" (fizzBuzz 15 22 10 9)))
-              )
+  (testing "15 22 10 9 should returr FizzBuzz 22 Buzz Fizz")
+  (is (= "FizzBuzz 22 Buzz Fizz" (fizzBuzz 15 22 10 9))))
