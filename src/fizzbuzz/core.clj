86e1cab945fdf95515722ca127522b7c9d01c359
Kevin Hantzen <kevin.hantzen@gmail.com>
2020-05-17 23:55:03 +0200

    refactor rule set

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,25 +1,31 @@
 (ns fizzbuzz.core)
 
-(defn validate [fbRule] (:if_validate fbRule))
-(defn returnValue [orDefault] #(:then_return % orDefault))
+(defn filter-non-null [arr] (filter #(not (nil? %)) arr))
+(defn join_or_default [orDefault] #(if (empty? %) (str orDefault) (apply str %)))
+
 
 (defn create-fizzBuzz [fizzBuzzRules]
   (fn fb?
     ([arg]
      (->
-      (filter #((validate %)  arg) fizzBuzzRules)
-      (first)
-      ((returnValue (str arg)))))
+      (map #(% arg) fizzBuzzRules)
+      (filter-non-null)
+      ((join_or_default arg))
+    ))
 
-    ([arg & next] (str (fb? arg) " " (apply fb? next)))))
+    ([arg & next] (str (fb? arg) " " (apply fb? next))))
+)
 
-(defn create_divide_by [value] #(= 0 (mod % value)))
-(def divBy3? (create_divide_by 3))
-(def divBy5? (create_divide_by 5))
+
+(defn create_is_divisible_by [value]  #(= 0 (mod % value)))
+  
+(def divBy3? (create_is_divisible_by 3))
+(def divBy5? (create_is_divisible_by 5))
+  
+(defn fizz-buzz-rule [predicate return-value] #(when (predicate %) return-value) )
 
 (def classic-fizzBuzz
-  [{:if_validate  #(and (divBy5? %) (divBy3? %))       :then_return "FizzBuzz"}
-   {:if_validate  #(and (divBy3? %) (not (divBy5? %))) :then_return "Fizz"}
-   {:if_validate  #(and (divBy5? %) (not (divBy3? %))) :then_return "Buzz"}])
+  [ (fizz-buzz-rule divBy3? "Fizz") (fizz-buzz-rule divBy5? "Buzz") ] 
+)
 
 (def fizzBuzz (create-fizzBuzz classic-fizzBuzz))
