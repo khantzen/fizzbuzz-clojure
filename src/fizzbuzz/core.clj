ec38c01b6d88e6d49a3215c34d05e527640fe4e8
Kevin Hantzen <kevin.hantzen@gmail.com>
2020-05-19 21:18:06 +0200

    trying some stuff using partial

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,29 +1,30 @@
 (ns fizzbuzz.core)
 
 (defn filter-non-null [arr] (filter #(not (nil? %)) arr))
-(defn join_or_default [orDefault] #(if (empty? %) (str orDefault) (apply str %)))
+(defn join-or-default [orDefault] #(if (empty? %) (str orDefault) (apply str %)))
+(defn map-rules-on [rules arg] (map #(% arg) rules))
 
-(defn create-fizzBuzz [fizzBuzzRules]
+(defn create-fizzBuzz [fizzBuzz-rules]
   (fn fb?
     ([arg]
-     (->
-      (map #(% arg) fizzBuzzRules)
+     (-> 
+      (map-rules-on fizzBuzz-rules arg)
       (filter-non-null)
-      ((join_or_default arg))
+      ((join-or-default arg))
     ))
 
     ([arg & next] (str (fb? arg) " " (apply fb? next))))
 )
 
-(defn create_is_divisible_by [value]  #(= 0 (mod % value)))
-  
-(def divBy3? (create_is_divisible_by 3))
-(def divBy5? (create_is_divisible_by 5))
-  
+
+(defn can-be-divided-by? [value]
+  (let [check (fn [q n] (= 0 (mod n q)) )]
+    (partial check value)))
+
 (defn fizz-buzz-rule [predicate return-value] #(when (predicate %) return-value) )
 
 (def classic-fizzBuzz
-  [ (fizz-buzz-rule divBy3? "Fizz") (fizz-buzz-rule divBy5? "Buzz") ] 
+  [ (fizz-buzz-rule (can-be-divided-by? 3) "Fizz") (fizz-buzz-rule (can-be-divided-by? 5) "Buzz") ] 
 )
 
 (def fizzBuzz (create-fizzBuzz classic-fizzBuzz))
